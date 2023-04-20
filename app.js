dfa02ad8c12cc725d020436dc2ce963319d4f89e
Vedran Djokic <vedran_djokic12@hotmail.com>
2023-04-20 22:24:53 +0200

    Part VII - Error Handling Middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ dotenv.config();
 const photoRoutes = require("./routes/photoRoutes");
 const userRoutes = require("./routes/userRoutes");
 const favoritesRoutes = require("./routes/favoritesRoutes");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,6 +21,7 @@ app.get("/", async (req, res) => {
 app.use("/api/photos", photoRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/favorites", favoritesRoutes);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT 3000`);
